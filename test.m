a65f80f2e568974a8dd389f631de0d6c78e866f1
Bruce Li <isanoob1897@outlook.com>
2025-10-26 08:55:42 -0700

    add some code

--- test.m
+++ test.m
@@ -1,10 +1,12 @@
 #import <Foundation/Foundation.h>
 
 @interface Person : NSObject
- 
+// Objective-C types requires * before variable name (i think because they are pointers?)
 @property NSString *firstName;
 @property NSString *lastName;
- 
+@property NSNumber *age;
+// same purpose as above, but simpler
+@property int yearOfBirth;
 @end
 
 int main(void) {
